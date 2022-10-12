377908af8e94921d1c0ac80e4d3f1a3d8a3d6506
Kauserul <kauserulislam0055@gmail.com>
2022-10-12 13:51:27 +0600

    deploy replace done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import './Statistics.css'
 
 const Statistics = () => {
     const quiz = useLoaderData()
     const quizData = quiz.data
     console.log(quizData)
     return (
-        <div className='container mt-5 d-flex justify-content-center'>
-            
+        <div className='container mt-5 d-flex justify-content-center line-chart'>         
             <LineChart width={500} height={400} data={quizData}>
                 <Line type="monotone" dataKey="total" stroke="#82ca9d" />
                 <XAxis dataKey="name" />
