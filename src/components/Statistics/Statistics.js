9f5bc542947c5a866c173e6c88343a7db446ce30
Kauserul <kauserulislam0055@gmail.com>
2022-10-12 13:55:56 +0600

    responsvie done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -8,14 +8,17 @@ const Statistics = () => {
     const quizData = quiz.data
     console.log(quizData)
     return (
-        <div className='container mt-5 d-flex justify-content-center line-chart'>         
-            <LineChart width={500} height={400} data={quizData}>
-                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip></Tooltip>
-                <Legend></Legend>
-            </LineChart>
+        <div className='container mt-5 d-flex justify-content-center line-chart'>
+            <ResponsiveContainer width="80%" height={400}>
+
+                <LineChart width={500} height={400} data={quizData}>
+                    <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip></Tooltip>
+                    <Legend></Legend>
+                </LineChart>
+            </ResponsiveContainer>
         </div>
     );
 };
